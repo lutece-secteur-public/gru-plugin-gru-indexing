227f751d5ebcfc9eb907c0e1f63bf4e661f16b7c
TrainE <erwan.train@atos.net>
2017-01-10 15:09:33 +0100

    GRUSTOREEL-26 : set the number of search results of customer configurable

--- src/java/fr/paris/lutece/plugins/grustorageelastic/business/ElasticConnexion.java
+++ src/java/fr/paris/lutece/plugins/grustorageelastic/business/ElasticConnexion.java
@@ -41,8 +41,6 @@ import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
-import com.mysql.jdbc.StringUtils;
-
 import fr.paris.lutece.plugins.grustorageelastic.util.constant.GRUElasticsConstants;
 import fr.paris.lutece.portal.service.util.AppLogService;
 import fr.paris.lutece.portal.service.util.AppPropertiesService;
@@ -52,6 +50,8 @@ import fr.paris.lutece.util.httpaccess.HttpAccessException;
 import java.io.IOException;
 
 import java.util.Map;
+
+import org.apache.commons.lang.StringUtils;
 
 
 /**
@@ -89,7 +89,7 @@ public final class ElasticConnexion
      */
     public static String getESParam( String strPath, String strSpecif )
     {
-        String path = ( StringUtils.isNullOrEmpty( strPath ) ) ? "" : AppPropertiesService.getProperty( strPath );
+        String path = ( StringUtils.isBlank( strPath ) ) ? "" : AppPropertiesService.getProperty( strPath );
         String tmp = AppPropertiesService.getProperty( GRUElasticsConstants.PATH_ELK_SERVER ) +
             AppPropertiesService.getProperty( GRUElasticsConstants.PATH_ELK_PATH ) + path + strSpecif;
 
@@ -239,6 +239,10 @@ public final class ElasticConnexion
         filtered.set( "filter", filter );
 
         tmp.set( "filtered", filtered );
+        if( StringUtils.isNotBlank( AppPropertiesService.getProperty( GRUElasticsConstants.SIZE_ELK_SEARCH_PARAM_VALUE ) ) )
+        {
+            root.put( "size", AppPropertiesService.getProperty( GRUElasticsConstants.SIZE_ELK_SEARCH_PARAM_VALUE ) );
+        }
         root.set( "query", tmp );
 
         return mapper.writeValueAsString( root );
@@ -279,6 +283,10 @@ public final class ElasticConnexion
         filter.set( "bool", bool );
         filtered.set( "filter", filter );
         query.set( "filtered", filtered );
+        if( StringUtils.isNotBlank( AppPropertiesService.getProperty( GRUElasticsConstants.SIZE_ELK_SEARCH_PARAM_VALUE ) ) )
+        {
+            root.put( "size", AppPropertiesService.getProperty( GRUElasticsConstants.SIZE_ELK_SEARCH_PARAM_VALUE ) );
+        }
         root.set( "query", query );
 
         return mapper.writeValueAsString( root );
@@ -317,6 +325,10 @@ public final class ElasticConnexion
         bool.set( "should", should );
         and.set( "bool", bool );
         query.set( "and", and );
+        if( StringUtils.isNotBlank( AppPropertiesService.getProperty( GRUElasticsConstants.SIZE_ELK_SEARCH_PARAM_VALUE ) ) )
+        {
+            root.put( "size", AppPropertiesService.getProperty( GRUElasticsConstants.SIZE_ELK_SEARCH_PARAM_VALUE ) );
+        }
         root.set( "query", query );
 
         return mapper.writeValueAsString( root );
